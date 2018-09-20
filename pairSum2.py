1dd5fa996c71cd369a8fb8fbe79abbb5378c77ca
muokicaleb <muokicaleb@gmail.com>
2018-09-20 08:51:50 +0300

    Given a list find a pair of numbers that add up to 8

---
+++ pairSum2.py
@@ -0,0 +1,35 @@
+'''
+Given a list find a pair of numbers that add up to 8
+
+'''
+
+ans = 8
+
+listSum = [1, 2, 4, 6]
+sortedList = sorted(listSum)
+lengthList = len(sortedList)
+
+
+def calc(sortedList):
+    counter = 0
+    start = 0
+    stop = lengthList - 1
+    sumPair = sortedList[start] + sortedList[stop]
+    while (sumPair != ans) and (counter <= len(sortedList)):
+        sumPair = sortedList[start] + sortedList[stop]
+        if sumPair > ans:
+            # del sortedList[-1]
+            stop = stop - 1
+        elif sumPair < ans:
+            # del sortedList[0]
+            start = start + 1
+        counter = counter + 1
+        print(counter)
+
+    if (sumPair == ans) and (start != stop):
+        print("The answer is " + str(sortedList[start]) + " and " + str(sortedList[stop]))
+    else:
+        print("No answer")
+
+
+calc(sortedList)
